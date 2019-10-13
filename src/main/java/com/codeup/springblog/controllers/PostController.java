5fd7c9fc48ebb0b91d5b6d7a626c7e28a84eb96b
Kevin Arnall <kevinjarnall@gmail.com>
2019-10-13 18:18:39 -0500

    Post Controller

---
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts() {
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postsById(@PathVariable int id) {
+        return "view an individual post";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String postPost() {
+        return "create a new post";
+    }
+}
